63f63f587119b0f0a7f9b143cacff7c1c4df60fb
Charantej01 <tej92401@gmail.com>
2024-05-07 20:48:16 +0530

    new para

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let displayValue = '';
+
+function append_Number(number) {
+  displayValue += number;
+  updateDisplay();
+}
+
+function appendOperator(operator) {
+  displayValue += operator;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = '';
+  updateDisplay();
+}
+
+function calculateResult() {
+  try {
+    const result = eval(displayValue);
+    displayValue = result.toString();
+    updateDisplay();
+  } catch (error) {
+    displayValue = 'Error';
+    updateDisplay();
+  }
+}
+
+function updateDisplay() {
+  document.getElementById('display').value = displayValue;
+}
